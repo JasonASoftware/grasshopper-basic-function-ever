a2e2e574066dcb4ac4b7141609dbb3e76b4d6faa
Jason Ascencio <101837083+JasonASoftware@users.noreply.github.com>
2022-11-22 23:30:18 -0500

    created js file

---
+++ debugging.js
@@ -0,0 +1,12 @@
+
+//error bug default code
+// function addFive(num) {
+//   var total = num + 5
+//   return num
+// }
+
+//debugged it by changing the variable its going to return from num to total
+function addFive(num) {
+  var total = num + 5
+  return total
+}
